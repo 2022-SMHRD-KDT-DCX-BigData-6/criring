a7e3e99b0ca8f588eedc06350a382d8de2121f98
smhrd <smhrd@DESKTOP-7A6J83M>
2023-05-30 11:47:04 +0900

    index, login 및 데이터베이스 및 각 경로수정

--- clearing/src/main/java/com/clearing/model/MemberDAO.java
+++ clearing/src/main/java/com/clearing/model/MemberDAO.java
@@ -2,16 +2,40 @@ package com.clearing.model;
 
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionManager;
+
+import com.clearing.database.SqlSessionManager;
 
 public class MemberDAO {
-	SqlSessionFactory sqlSessionFactory = com.clearing.database.SqlSessionManager.getSqlSession();
+	SqlSessionFactory sqlSessionFactory = SqlSessionManager.getSqlSession();
 	SqlSession sqlSession = sqlSessionFactory.openSession(true);
-	public MemberDTO loginCheck(MemberDTO dto) {
+	
+	public int joinMember(MemberDTO dto) {
+		SqlSession sqlSession = sqlSessionFactory.openSession(true);
+		int result = sqlSession.insert("joinMember", dto);
+		sqlSession.close();
+		return result;
+	}
+	
+	public int joinStore(StoreDTO dto) {
+		SqlSession sqlSession = sqlSessionFactory.openSession(true);
+		int result = sqlSession.insert("joinStore", dto);
+		sqlSession.close();
+		return result;
+	}
+	
+	public MemberDTO loginChk(MemberDTO dto) {
+		SqlSession sqlSession = sqlSessionFactory.openSession(true);
 		MemberDTO member = null;
-		
-		member = sqlSession.selectOne("com.clearing.database.MemberMapper.loginchk", dto);
+		member = sqlSession.selectOne("loginChk", dto);
 		sqlSession.close();
 		return member;
+	}
+	
+	public int loginCount(MemberDTO dto) {
+		SqlSession sqlSession = sqlSessionFactory.openSession(true);
+		int result = sqlSession.selectOne("loginCount", dto);
+		
+		sqlSession.close();
+		return result;
 	}
 }
---
+++ clearing/src/main/java/com/clearing/model/StoreDAO.java
@@ -0,0 +1,27 @@
+package com.clearing.model;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.clearing.database.SqlSessionManager;
+
+public class StoreDAO {
+	SqlSessionFactory sqlSessionFactory = SqlSessionManager.getSqlSession();
+	SqlSession sqlSession = sqlSessionFactory.openSession(true);
+
+	public int insertLaundry(LaundryDTO ldto) {
+		SqlSession sqlSession = sqlSessionFactory.openSession(true);
+		int result = 0;
+		result = sqlSession.insert("registLaundry", ldto);
+		sqlSession.close();
+		return result;
+	}
+
+	public StoreDTO loginChk(StoreDTO sdto) {
+		SqlSession sqlSession = sqlSessionFactory.openSession(true);
+		StoreDTO storeMember = null;
+		storeMember = sqlSession.selectOne("storeLoginChk", sdto);
+
+		return storeMember;
+	}
+}
